830ddb27554bb4d0e69aba3b01d65984b3b44ae5
Stefaan Lippens <stefaan@netlog.com>
2011-01-11 13:58:57 +0100

    a bit of clean up and comment tweaking

--- git-svn-problem-scenario.sh
+++ git-svn-problem-scenario.sh
@@ -1,77 +1,74 @@
 #!/bin/bash
 
-# Show all commands as we go allong
+# Show all commands as we go allong.
 set -x
 
-# clean up before start
+# Clean up output from previous runs before we start.
 rm -fr svn-repo svn-wc git-wc
 
-# Get absulute path of current working directory
+# Get absulute path of current working directory.
 root=$(pwd)
 
-# Set up SVN repo
+# Set up initial SVN repo.
 svnadmin create svn-repo
 svn checkout file://$root/svn-repo svn-wc
 cd $root/svn-wc
+# Create trunk/branches/tags layout.
 svn mkdir trunk
 svn mkdir branches
 svn mkdir tags
 svn commit -m "Repo layout"
-
-# Work on trunk
-cd $root/svn-wc/trunk
-echo "Hello world" > readme.txt
-svn add readme.txt
+# Create a readme file.
+echo "Hello world" > trunk/readme.txt
+svn add trunk/readme.txt
 svn commit -m "Created readme.txt"
-
-# Create a SVN-branch
-cd $root/svn-wc
+# Create a SVN-branch from trunk.
 svn copy trunk branches/branch
 svn commit -m "Created SVN branch"
 
-# Git-svn clone/checkout
+# Git-svn clone/checkout.
 cd $root
 git svn clone -s --prefix=svn/ file://$root/svn-repo git-wc
 
-# Work in git: create tracking branche
+# Work in git: create tracking branch.
 cd $root/git-wc
 git branch --track svn-branch svn/branch
 
-# Work on master (aka trunk) and commit to SVN
+# Work on master (aka trunk) and commit to SVN.
 git checkout master
 echo "this is from master" > frommaster.txt
 git add frommaster.txt
 git commit -m "added frommaster.txt"
 git svn dcommit
 
-# Work on svn-branch (SVN branch) and commit to SVN
+# Work on svn-branch (SVN branch) and commit to SVN.
 git checkout svn-branch
 echo "this is from branch" > frombranch.txt
 git add frombranch.txt
 git commit -m "added frombranch.txt"
 git svn dcommit
 
-# Merge master in branch and commit to SVN
+# Merge master in branch and commit to SVN.
 git checkout svn-branch
 git merge master
 git svn dcommit
 
-# Merge branch in master
+# Merge branch in master.
 git checkout master
 git merge svn-branch
 
-# Work in master/trunk
+# Work in master.
 git checkout master
 echo "more stuff from master" > morefrommaster.txt
 git add morefrommaster.txt
 git commit -m "more stuff from master"
 
 # Try to commit from master to SVN trunk
-# but this fails: commits to SVN branch
+# but this fails: commits to SVN branch instead.
 git checkout master
 git svn dcommit
 
-# Check SVN wc
+# Check SVN wc.
 cd $root/svn-wc
 svn update
 
