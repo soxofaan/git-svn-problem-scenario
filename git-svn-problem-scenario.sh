f1790a8827c8a81704033a63ce0497aaf6ea7069
Stefaan Lippens <stefaan@netlog.com>
2011-01-11 13:51:02 +0100

    removed git aliases

--- git-svn-problem-scenario.sh
+++ git-svn-problem-scenario.sh
@@ -11,7 +11,7 @@ root=$(pwd)
 
 # Set up SVN repo
 svnadmin create svn-repo
-svn co file://$root/svn-repo svn-wc
+svn checkout file://$root/svn-repo svn-wc
 cd $root/svn-wc
 svn mkdir trunk
 svn mkdir branches
@@ -26,7 +26,7 @@ svn commit -m "Created readme.txt"
 
 # Create a SVN-branch
 cd $root/svn-wc
-svn cp trunk branches/branch
+svn copy trunk branches/branch
 svn commit -m "Created SVN branch"
 
 # Git-svn clone/checkout
@@ -38,41 +38,41 @@ cd $root/git-wc
 git branch --track svn-branch svn/branch
 
 # Work on master (aka trunk) and commit to SVN
-git co master
+git checkout master
 echo "this is from master" > frommaster.txt
 git add frommaster.txt
-git ci -m "added frommaster.txt"
+git commit -m "added frommaster.txt"
 git svn dcommit
 
 # Work on svn-branch (SVN branch) and commit to SVN
-git co svn-branch
+git checkout svn-branch
 echo "this is from branch" > frombranch.txt
 git add frombranch.txt
-git ci -m "added frombranch.txt"
+git commit -m "added frombranch.txt"
 git svn dcommit
 
 # Merge master in branch and commit to SVN
-git co svn-branch
+git checkout svn-branch
 git merge master
 git svn dcommit
 
 # Merge branch in master
-git co master
+git checkout master
 git merge svn-branch
 
 # Work in master/trunk
-git co master
+git checkout master
 echo "more stuff from master" > morefrommaster.txt
 git add morefrommaster.txt
-git ci -m "more stuff from master"
+git commit -m "more stuff from master"
 
 # Try to commit from master to SVN trunk
 # but this fails: commits to SVN branch
-git co master
+git checkout master
 git svn dcommit
 
 # Check SVN wc
 cd $root/svn-wc
-svn up
+svn update
 
 
